1e7a34cb0b331820f3c63c7e4f032ad0ba2629c9
Omer Holtzman <omer.holtzman@gmail.com>
2022-12-02 16:15:37 +0200

    added basic conv edge detection main basic script

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from IO import read_image, GRAYSCALE
+from object_detector import ObjectDetector
 
 FILE_PATH = ""
 
 if __name__ == '__main__':
     image = read_image(FILE_PATH, GRAYSCALE)
+    found_objects = ObjectDetector().detect_objects(image)
 
